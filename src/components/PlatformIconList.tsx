883334c21107124e736cf531ad0186fc52583d31
Nicholas Poon <poontzejian@gmail.com>
2024-08-03 16:44:02 +0800

    Displaying Icon platform

---
+++ src/components/PlatformIconList.tsx
@@ -0,0 +1,48 @@
+import {
+  FaWindows,
+  FaPlaystation,
+  FaXbox,
+  FaApple,
+  FaLinux,
+  FaAndroid,
+} from "react-icons/fa";
+import { MdPhoneIphone } from "react-icons/md";
+import { SiNintendo } from "react-icons/si";
+import { BsGlobe } from "react-icons/bs";
+import { HStack, Icon, Text } from "@chakra-ui/react";
+import { Platform } from "../hooks/useGames";
+import { IconType } from "react-icons";
+
+interface Props {
+  platforms: Platform[];
+}
+
+const PlatformIconList = ({ platforms }: Props) => {
+  // When wrap in [] we are defining index signture
+  // Any numbers of keys, dont care about the names
+  const iconMap: { [key: string]: IconType } = {
+    // name: PlayStation
+    // slug: playstation  rely on this because it is not suppose to change
+
+    pc: FaWindows,
+    playstation: FaPlaystation,
+    xbox: FaXbox,
+    nintendo: SiNintendo,
+    mac: FaApple,
+    linux: FaLinux,
+    android: FaAndroid,
+    ios: MdPhoneIphone,
+    web: BsGlobe,
+  };
+  return (
+    // default them.space value
+    // Use numeric to be consistent
+    <HStack marginY={1}>
+      {platforms.map((platform) => (
+        <Icon as={iconMap[platform.slug]} color="gray.500" />
+      ))}
+    </HStack>
+  );
+};
+
+export default PlatformIconList;
