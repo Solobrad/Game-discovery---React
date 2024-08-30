a0e92fd6f50906a1791139569c6b8db261438a46
Nicholas <poontzejian@gmail.com>
2024-08-30 08:45:12 +0800

    api bug

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -5,6 +5,6 @@ const apiKey = import.meta.env.VITE_RAWG_API_KEY;
 export default axios.create({
     baseURL: 'https://api.rawg.io/api',
     params: {
-        key: apiKey
+        key: '6e910b1993ac4652b24d3117a9ba7c8b'
     }
 });
